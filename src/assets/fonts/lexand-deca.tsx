5a9d843aece2576bf77f93e2bf87d9b45c73b91c
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-08-26 17:56:13 -0300

    Criação da pasta Assets, instalação de fontes e icons

---
+++ src/assets/fonts/lexand-deca.tsx
@@ -0,0 +1,3 @@
+import "@fontsource/lexend-deca"; // Defaults to weight 400
+import "@fontsource/lexend-deca/400.css"; // Specify weight
+import "@fontsource/lexend-deca/400-italic.css"; // Specify weight and style
