081738a1e7daa922bb23ed1631c3bd057dbdbf15
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-08-27 12:21:27 -0300

    Atualização App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/metaseguranca.jpg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         Meta Segurança Eletrônica
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.metasegurancaeletronica.com.br/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Acesse o site ridiculo
         </a>
       </header>
     </div>
