a7744a4989a6e57f346a71f9fb21f2958b92f429
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-08-28 18:08:33 -0300

    Inicio da criação do Navbar

---
+++ src/components/menu/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <a href="/" className="logo">Logo</a>
+
+      <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Portifolio</a>
+        <a href="/">Services</a>
+        <a href="/">Contact</a>
+        <a href="/"></a>
+      </nav>
+
+    </header>
+  )
+}
+
+export default Navbar
