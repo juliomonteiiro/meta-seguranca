c894b3bd31f2f9367ba0c0db2544dfc253d964d1
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-08-29 19:29:24 -0300

    Criação do componente input

--- src/components/form/input/index.jsx
+++ src/components/form/input/index.jsx
@@ -1,2 +1,26 @@
-import { InputHTMLAttributes, useEffect, useState } from "react";
+import  React  from "react";
+import { useEffect, useState } from "react";
 import "./main.css"
+
+
+export function InputText({ className, errorMessage, showError, ...props }) {
+
+  const [error, setError] = useState(showError);
+  const [message, setMessage] = useState(errorMessage);
+
+  useEffect(() => {
+    setError(showError);
+    setMessage(errorMessage);
+  }, [showError, errorMessage]);
+
+  return (
+    <>
+      <div className={className + " div-input " + (error ? "error" : "")}>
+        <input
+          {...props}
+        />
+      </div>
+      {showError ? <p className="error-message">{message}</p> : <></>}
+    </>
+  )
+}
