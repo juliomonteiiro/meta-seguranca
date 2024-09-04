8c808115c06ff289f6220ec140fc31705a5ce73b
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-09-04 20:34:28 -0300

    Inicio da criação do footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './components/menu/Navbar'
 import { Budget } from './pages/budget/budget'
 import { Services } from './pages/services/services'
+import Footer  from './pages/footer/footer'
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <Budget/>
+      <Budget id="contato"/>
       <Services/>
+      <Footer />
     </div>
   )
 }
---
+++ src/pages/footer/footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+   <div className="footer">
+            <div className="social-media">
+
+            </div>
+        <div className="section">
+            <div className="sb-section">
+                <div className="maps">
+
+                </div>
+            </div>
+            <div className="sb-section">
+
+
+            </div>
+        </div>
+
+   </div>
+  )
+}
+
+export default Footer
