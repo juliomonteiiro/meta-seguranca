22895067614f9c2d8ac8e7fad3ec7fd8eb7558e7
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-09-04 17:51:38 -0300

    Importação de bibliotecas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/menu/Navbar'
 import { Budget } from './pages/budget/budget'
 import { Services } from './pages/services/services'
