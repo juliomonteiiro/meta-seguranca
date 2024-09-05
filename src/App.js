bbce637154c1daa0b08448db8ac8bab169d66e25
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-09-05 18:08:48 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Navbar from './components/menu/Navbar'
 import { Budget } from './pages/budget/budget'
 import { Services } from './pages/services/services'
-import Footer  from './pages/footer/footer'
+import Footer  from './components/footer/footer'
 import { Home } from './pages/home/home.page'
 
 
