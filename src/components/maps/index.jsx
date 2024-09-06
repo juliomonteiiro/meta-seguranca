81ec65585eb37eb04ff2db1e67840141b31c412c
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-09-06 14:39:01 -0300

    Modificações maps

--- src/components/maps/index.jsx
+++ src/components/maps/index.jsx
@@ -1,28 +1,17 @@
 import React from "react";
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import './main.css'
 
 
-const MapPage = () => {
-    const { isLoaded } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: "AIzaSyAM3TBO_1oITW8z9Yw5VpD5dVYRdM8FrTw"
-      });
-    return <div className="map">
-        {isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={{width: '100%', height: '100 '}}
-        center={{
-            lat: -22.8824454,
-            lng: -47.0597047
-        }}
-        zoom={15}
-      ></GoogleMap>
-  ) : (<></>
+const Maps = () => {
 
-  )}
-    </div>;
-};
+  return(
+    
+    <div className="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14703.525065027357!2d-47.0627517!3d-22.8808441!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8c608b02b51fd%3A0x82ec624834c4ba82!2sMeta%20Seguran%C3%A7a%20Eletr%C3%B4nica!5e0!3m2!1spt-BR!2sbr!4v1725623035357!5m2!1spt-BR!2sbr" width="300" height="250" border-radius="10" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  )
+
+}
 
 
-export default MapPage;
+export default Maps
