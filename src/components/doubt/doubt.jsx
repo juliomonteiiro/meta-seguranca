974c609bcc9e67f123d52e5c018f6700e6fa5ea7
Andre Dantas <andreluissousaa10@gmail.com>
2024-09-10 18:32:30 -0300

    Aba de duvidas

--- src/components/doubt/doubt.jsx
+++ src/components/doubt/doubt.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./main.css";
-import {Button} from "../form/button/index"
+import { Button } from "../form/button/index";
+import duvida from '../../assets/images/duvida.png';
 
-export function Doubt(){
-    return(
+export function Doubt() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Verifica a largura inicial da tela
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      {windowWidth <= 600 ? (
+        <a href="/outra-tela" className="Doubt-icon">
+          <img src={duvida} alt="Ícone de dúvida" />
+        </a>
+      ) : (
         <div className="Container">
-            <p>Tem alguma duvida ou quer saber mais a respeito?</p>
-                <Button>Fale Conosco</Button>
+          <p>Tem alguma dúvida ou quer saber mais a respeito?</p>
+          <Button>Fale Conosco</Button>
         </div>
-    )
+      )}
+    </>
+  );
 }
