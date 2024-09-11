6ad0fa019836d9434f88ff335a0febe80fdc9b4f
Andre Dantas <andreluissousaa10@gmail.com>
2024-09-11 12:19:34 -0300

    Serviços

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react'
 import Navbar from './components/menu/Navbar'
 import { Budget } from './pages/budget/budget'
 import { Services } from './pages/services/services'
@@ -9,9 +8,9 @@ const App = () => {
   return (
     <div>
       <Home />
+      <Services/>
       <Navbar/>
       <Budget id="contato"/>
-      <Services/>
       <Footer />
      
     </div>
--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import "./services.css";
+import CFTV from '../../assets/images/CFTV.png';
+import ALARME from '../../assets/images/ALARME.png';
+import CONTROLE from '../../assets/images/CONTROLE-ACESSO.png';
+import CERCA from '../../assets/images/CERCA.png';
 
-export function Services(){
-    return(
-        <div className="Services-Container">
-            <h1>Serviços</h1>
-        </div>
-    )
+export function Services() {
+  // Lista de serviços
+  const services = [
+    {
+      image: CFTV,
+      title: "CFTV",
+      text: "Instalamos sistemas de alarme avançados após análise personalizada, conectados à nossa central 24 horas.",
+    },
+    {
+      image: ALARME,
+      title: "Alarme",
+      text: "Após avaliação, instalamos câmeras de alta tecnologia nos pontos estratégicos de seu imóvel. ",
+    },
+    {
+      image: CONTROLE,
+      title: "Controle de acesso",
+      text: "Instalamos cercas elétricas para proteção eficaz de seu imóvel.A cerca elétrica previne invasões com um sistema eficiente.",
+    },
+    {
+      image: CERCA,
+      title: "Proteção perimetral",
+      text: "solução ideal para o controle de fluxo e maior segurança em condomínios, empresas e indústrias.", 
+    },
+  ];
+
+  return (
+    <div className="Services-Container">
+      <h1>Serviços</h1>
+      <div className="Services-Grid">
+        {services.map((service, index) => (
+          <div className="card" key={index}>
+            <div className="icon">
+              <img src={service.image} alt={service.title} />
+            </div>
+            <h2>{service.title}</h2>
+            <p>{service.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
