c41b617c56186f9d1dac73b52065df0e662cd324
Julio Monteiro <150204882+juliomonteiiro@users.noreply.github.com>
2024-10-22 19:21:55 -0300

    Update registration.php

--- backend/registration.php
+++ backend/registration.php
@@ -1,6 +1,5 @@
 <?php 
 
-
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
