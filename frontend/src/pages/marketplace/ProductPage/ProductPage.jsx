9ce05d1d836247da542818e2c03eeecd215f49b5
andreluiisdantas <andreluissousaa10@gmail.com>
2024-10-24 17:14:32 -0300

    Feat: ProductPage

---
+++ frontend/src/pages/marketplace/ProductPage/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './ProductPage.css';
+
+export function ProductPage(){
+    return(
+        <div className="ProductPage-Container">
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default ProductPage;
