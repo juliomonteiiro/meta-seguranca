aa6605b509d41223a490f28994942b9385867fe3
Andre Dantas <andreluissousaa10@gmail.com>
2024-10-24 23:38:01 -0300

    Feat: ProductPage

--- frontend/src/pages/marketplace/ProductPage/ProductPage.jsx
+++ frontend/src/pages/marketplace/ProductPage/ProductPage.jsx
@@ -4,7 +4,25 @@ import './ProductPage.css';
 export function ProductPage(){
     return(
         <div className="ProductPage-Container">
-            <h1>Teste</h1>
+            <div className="ProductPage-Header">
+                {/* Nome do produto */}
+            </div>
+            <div className="ProductPage-Content">
+                <div className="ProductPage-Image">
+                    {/* Imagem do produto */}
+                </div>
+                <div className="ProductPage-Tools">
+                    <div className="ProductPage-Title">
+                        {/* Modelo do equipamento */}
+                    </div>
+                    <div className="ProductPage-Buttons">
+                        {/* Botões de ação */}
+                    </div>
+                </div>
+            </div>
+            <div className="ProductPage-Description">
+                {/* Descrição das funcionalidades do projeto */}
+            </div>
         </div>
     )
 }
