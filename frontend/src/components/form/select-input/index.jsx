b9206d05e10c4ed9fcf18e72ae40acf5422414db
juliomonteiiro <julioalexsandro.monteiro13@gmail.com>
2024-11-03 20:55:11 -0300

    update select-input

--- frontend/src/components/form/select-input/index.jsx
+++ frontend/src/components/form/select-input/index.jsx
@@ -1,7 +1,6 @@
-
 import React from "react";
 import Select from 'react-select';
-import "./main.css"
+import "./main.css";
 
 export function SelectInput({ className, options, showError, errorMessage, handleChange, placeholder, optionSelected }) {
   return (
@@ -10,11 +9,11 @@ export function SelectInput({ className, options, showError, errorMessage, handl
         className={`select-container ${className} select ${showError ? "error" : ""}`}
         classNamePrefix="react-select"
         options={options}
-        value={optionSelected}
-        onChange={handleChange}
+        value={options.find(option => option.value === optionSelected)} // Encontra o objeto correto com base no value
+        onChange={(selectedOption) => handleChange({ target: { name: "tipo_servico", value: selectedOption.value } })}
         placeholder={placeholder}
       />
       {showError && <p className="error-message">{errorMessage}</p>}
     </>
-  )
+  );
 }
