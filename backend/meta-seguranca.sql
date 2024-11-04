edd5a053c6630a2bd3653c6b5640f653d0db994f
Julio Monteiro <julioalexsandro.monteiro13@gmail.com>
2024-11-04 18:05:17 -0300

    backup sql

--- backend/meta-seguranca.sql
+++ backend/meta-seguranca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22/10/2024 às 23:11
+-- Tempo de geração: 04/11/2024 às 22:02
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `orcamentos`
+--
+
+CREATE TABLE `orcamentos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `empresa` varchar(255) DEFAULT NULL,
+  `cpf_cnpj` varchar(20) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `tipo_servico` varchar(255) DEFAULT NULL,
+  `solicitacao` text DEFAULT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `usuarios`
 --
 
@@ -31,19 +50,31 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
+  `senha` varchar(255) NOT NULL,
+  `foto_perfil` varchar(255) DEFAULT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `telefone` varchar(12) NOT NULL,
+  `data_nasc` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'julio', 'jsilva@multi360.com.br', 'L46t2LZNVR*5%U');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `foto_perfil`, `cpf`, `telefone`, `data_nasc`) VALUES
+(1, 'julio', 'jsilva@multi360.com.br', '1', NULL, '', '', NULL),
+(2, 'Julio Alexsandro Monteiro da Silva', 'julioalexsandro.monteiro13@gmail.com', '$2y$10$yDcLZb.QZViEyCUCoYUPDe3VqR0ejavVFrEu3CpPPsg0W9ebODNZ2', NULL, '37859235897', '19991971956', '2005-10-13'),
+(3, 'Pafaro', 'pafarinho123@gmail.com', '$2y$10$wdNlNZZmdCVBU8QE90B5K.56qyRuEkJvm3QN7Cmr/SyJbvOEKYTUu', NULL, '', '', NULL);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `orcamentos`
+--
+ALTER TABLE `orcamentos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `usuarios`
@@ -57,10 +88,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `orcamentos`
+--
+ALTER TABLE `orcamentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
