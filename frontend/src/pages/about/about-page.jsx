b6dc0cd87d2aed028a04cec6a4a3854f4bf1cf54
Andre Dantas <andreluissousaa10@gmail.com>
2024-11-04 23:01:14 -0300

    Refactor: AboutPage

--- frontend/src/pages/about/about-page.jsx
+++ frontend/src/pages/about/about-page.jsx
@@ -1,42 +1,35 @@
 import React from "react";
 import "@fontsource/lexend-deca"; 
-import "./main.css"
-import about from "../../assets/images/about.png"
+import styles from "./About.module.css"; 
+import about from "../../assets/images/metaseguranca.png";
 
-export function About (){
-    return(
-        <div className="about">
-            <div className="section">
-                <div className="about-section texto">
-                    <h1>Sobre a <strong>Meta</strong></h1>
-                    <br />
-                    <p>
-                    Com mais de 30 anos de experiência no segmento de 
-                    <br />segurança e proteção, os profissionais da META 
-                    <br />seguem um padrão de qualidade diferenciado, por 
-                    <br />meio de consultoria, tecnologia de ponta e excelência 
-                    <br />nos serviços prestados.
-                    <br />Além de contar com o constante aperfeiçoamento da 
-                    <br />equipe, a META utiliza equipamentos e soluções de 
-                    <br />última geração dos mais conceituados fornecedores, 
-                    <br />pensando sempre na melhor opção custo-benefício 
-                    <br />para os seus clientes.
-                    <br />Apesar de estar localizada na cidade de Campinas, 
-                    <br />a empresa atende diversas cidades da região, como 
-                    <br />São Paulo, Araraquara, Americana, 
-                    <br />Santa Bárbara D´Oeste, Sumaré, Hortolândia e Jundiaí. 
-                    <br />Conta, também, com um estoque de peças para reposição 
-                    <br />em casos emergenciais, e possui uma central de 
-                    <br />monitoramento 24 horas com monitores e atendentes 
-                    <br />treinados e qualificados para atendimento imediato.
-                    </p>
-                </div>
-                <div className="about-section">
-                    <img src={about} alt="" />
-                </div>
-            </div>
+export function About() {
+  return (
+    <div className={styles.about}>
+        <div className={`${styles.aboutText} ${styles.texto}`}>
+          <h1>Sobre a <strong>Meta</strong></h1>
+          <p>
+            Com mais de 30 anos de experiência no segmento de 
+            segurança e proteção, os profissionais da META 
+            seguem um padrão de qualidade diferenciado, por 
+            meio de consultoria, tecnologia de ponta e excelência 
+            nos serviços prestados. Além de contar com o constante 
+            aperfeiçoamento da equipe, a META utiliza equipamentos 
+            e soluções de última geração dos mais conceituados fornecedores, 
+            pensando sempre na melhor opção custo-benefício para os seus clientes.
+            Apesar de estar localizada na cidade de Campinas, a empresa atende 
+            diversas cidades da região, como São Paulo, Araraquara, Americana, 
+            Santa Bárbara D´Oeste, Sumaré, Hortolândia e Jundiaí. Conta, também, 
+            com um estoque de peças para reposição em casos emergenciais, e possui 
+            uma central de monitoramento 24 horas com monitores e atendentes 
+            treinados e qualificados para atendimento imediato.
+          </p>
         </div>
-    );
+        <div className={styles.aboutImage}>
+          <img src={about} alt="Sobre a empresa Meta" />
+        </div>
+      </div>
+  );
 }
 
-export default About
+export default About;
